62858b153ef9fea0da08c98d3522edb583c2c003
Emre <ecak0002@student.monash.edu>
2020-08-23 20:23:05 +1000

    [initial merge]

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_Moot_Up.Models
+{
+    public class User
+    {
+        public string FirstName{ get; set; }
+        public string LastName{ get; set; }
+        public string Email{ get; set; }
+        public string StudyYear{ get; set; }
+        public string MootExperience{ get; set; }
+        public string TeammateMootExperience{ get; set; }
+        public string Goal{ get; set; }
+        public string HoursExpected{ get; set; }
+        public string WantToBeBarrister{ get; set; }
+        public string DateSubmitted{ get; set; }
+
+    }
+   
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Test_Moot_Up.Models;
+
+namespace Test_Moot_Up.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        public void OnPost()
+        {
+            var new_user = new User();
+            new_user.FirstName = Request.Form["given-name"];
+            new_user.LastName = Request.Form["family-name"];
+            new_user.Email = Request.Form["email"];
+            new_user.StudyYear = Request.Form["year"];
+            new_user.MootExperience = Request.Form["experience"];
+            new_user.TeammateMootExperience = Request.Form["teamExperience"];
+            new_user.Goal = Request.Form["goal"];
+            new_user.HoursExpected = Request.Form["time"];
+            new_user.WantToBeBarrister = Request.Form["role"];
+
+        }
+             
+
+    }
+
+    
+}
